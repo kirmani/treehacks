08743968e9dfc8710e4445d9555b600d28c8e778
Sean Kirmani <sean@kirmani.io>
2016-02-12 23:29:34 -0800

    Added MenuItem selection

--- android/TreeHacks/app/src/main/java/io/kirmani/tango/treehacks/MainActivity.java
+++ android/TreeHacks/app/src/main/java/io/kirmani/tango/treehacks/MainActivity.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -109,6 +110,22 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        int itemId = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_create:
+                // createSession();
+                return true;
+            case R.id.action_join:
+                // joinSession();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
