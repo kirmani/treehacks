b0fccc1886d80bc39627b7a1f1fdf6eeaff8d960
Sean Kirmani <sean@kirmani.io>
2016-02-13 01:51:55 -0800

    Added Volley Request tool

---
+++ android/TreeHacks/app/src/main/java/io/kirmani/tango/treehacks/HttpRequestUtil.java
@@ -0,0 +1,17 @@
+/*
+ * HttpRequestUtil.java
+ * Copyright (C) 2016 kirmani <sean@kirmani.io>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+package io.kirmani.tango.treehacks;
+
+public class HttpRequestUtil {
+    private static final String BASE_URL = "http://treehacks.kirmani.io/";
+
+    public HttpRequestUtil() {
+
+    }
+}
+
