e46928a4f36cf242bbfd3344abfcb296fb59c90f
Sean Kirmani <sean@kirmani.io>
2016-02-13 02:09:26 -0800

    Added Volley RequestQueue singleton

--- android/TreeHacks/app/src/main/java/io/kirmani/tango/treehacks/HttpRequestUtil.java
+++ android/TreeHacks/app/src/main/java/io/kirmani/tango/treehacks/HttpRequestUtil.java
@@ -7,8 +7,61 @@
 
 package io.kirmani.tango.treehacks;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class HttpRequestUtil {
     private static final String BASE_URL = "http://treehacks.kirmani.io/";
+
+    private static HttpRequestUtil mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mContext;
+
+    private HttpRequestUtil(Context context) {
+        mContext = context;
+    }
+
+    public static synchronized HttpRequestUtil getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new HttpRequestUtil(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request) {
+        getRequestQueue().add(request);
+    }
+
+    public static void createSession(String sessionId) {
+        String url = BASE_URL + "session/" + sessionId;
+        // JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, null,
+        //         new Response.Listener<JSONObject>() {
+        //             @Override
+        //             public void onResponse(JSONObject response) {
+        //                 // do something with response
+        //             }
+        // }, new Response.ErrorListener() {
+        //     @Override
+        //     public void onErrorResponse(VolleyError error) {
+        //         // handle error
+        //     }
+        // });
+    }
 
     public HttpRequestUtil() {
 
