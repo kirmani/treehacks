cb350a145cdcbbd7d25b5077f6fff93ceeb45c25
Sean Kirmani <sean@kirmani.io>
2016-02-13 14:15:37 +0000

    Added server files

--- public_html/app.py
+++ public_html/app.py
@@ -10,24 +10,67 @@ import json
 import os
 
 from flask import Flask
+from flask import request
+from flask import current_app
+
+DATAFILE = '/var/www/kirmani.io/treehacks/public_html/data.json'
 
 app = Flask(__name__, static_url_path='')
 app.secret_key = 'multi_tango_kirmani_io'
 
+@app.before_request
+def log_request():
+  print request.headers
+  print request.path
+  print request.method
+  print "DATA: " + request.data
+  #print "JSON: " + request.json
+
 @app.route('/')
 def Home():
   return 'Hello world!'
+
+@app.route('/session/', defaults={'id': None}, methods=['GET','POST'])
+@app.route('/session/<id>/', methods=['GET','POST','PUT'])
+def session(id):
+  if id is None:
+    if request.method == 'GET':
+      return "{\"error\": \"Invalid request. You must use a POST request to create new sessions\"}"
+    for key in request.values:
+      print key + ": " + request.values[key]
+    jsondata = request.json
+    serverdata = _LoadFile(DATAFILE);
+    if 'sessions' not in serverdata:
+      serverdata['sessions'] = {}
+    serverdata['sessions'][jsondata['name']] = jsondata['data']
+    _WriteFile(serverdata, DATAFILE) 
+  else:
+    if request.method == 'GET':
+      serverdata = _LoadFile(DATAFILE)
+      if id in serverdata['sessions']:
+        return json.dumps(serverdata['sessions'][id])
+    if request.method == 'PUT':
+        jsondata = request.json
+        serverdata = _LoadFile(DATAFILE)
+        for key in jsondata:
+          serverdata['sessions'][id][key] = jsondata[key]
+        _WriteFile(serverdata, DATAFILE)
+    return 'Use existing session'
+  return 'Session: %s' % id
 
 def _LoadFile(filename):
   try:
     with open(filename, 'r+') as f:
       return json.load(fp=f)
   except IOError:
+    print "Error"
     return {}
 
 def _WriteFile(data, filename):
   j = json.dumps(data, indent=4)
-  with open(filename, 'w') as f:
+  print "WRITE: " + j
+  print "FILE: " + filename
+  with open(filename, 'w+') as f:
     f.write(j)
 
 if __name__ == '__main__':
